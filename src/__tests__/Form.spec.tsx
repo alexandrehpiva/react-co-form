dca9debc18e384179378803f26be72bc489a53e9
alexandrehpiva <alexandrehpiva@gmail.com>
2021-05-13 23:39:06 -0300

    Add a simple render test

---
+++ src/__tests__/Form.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Form from './components/Form';
+
+describe('Form', () => {
+  it('should render form elements correctly', () => {
+    render(<Form />);
+  });
+});
---
+++ src/__tests__/components/Form.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FormEvent } from 'react';
+
+import useCoForm from '../../useCoForm';
+
+type FormProps = {
+  name: string;
+  password: string;
+};
+
+const initialValues: FormProps = {
+  name: '',
+  password: '',
+};
+
+const Form: React.FC = () => {
+  const { values, handleChange } = useCoForm<FormProps>({ initialValues });
+
+  return (
+    <form
+      data-testid="form"
+      onSubmit={(event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(values);
+      }}
+    >
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleChange}
+          value={values.name}
+        />
+      </div>
+      <div>
+        <label htmlFor="address">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+          value={values.password}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
