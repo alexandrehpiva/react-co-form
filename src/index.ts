9a89e9e63181c2907d3bfd4c9408b88069d43242
alexandrehpiva <alexandrehpiva@gmail.com>
2021-05-18 12:08:32 -0300

    Re-exporting types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { CoFormProps as CoFormPropsAlias, CoFormResp as CoFormRespAlias } from './types';
+
+/**
+ * Types
+ */
+export type CoFormProps<T> = CoFormPropsAlias<T>;
+export type CoFormResp<T> = CoFormRespAlias<T>
+
 /**
  * Hooks
  */
